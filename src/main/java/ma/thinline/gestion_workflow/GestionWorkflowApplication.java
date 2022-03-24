ecd6dd97318c366cb8ac3b2aadf72e09cf133112
hamza <benfaida.hamza2014@gmail.com>
2022-03-24 15:50:10 +0100

    commit2

--- src/main/java/ma/thinline/gestion_workflow/GestionWorkflowApplication.java
+++ src/main/java/ma/thinline/gestion_workflow/GestionWorkflowApplication.java
@@ -1,13 +1,21 @@
 package ma.thinline.gestion_workflow;
 
+import ma.thinline.gestion_workflow.modele.Utilisateur;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class GestionWorkflowApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GestionWorkflowApplication.class, args);
+        Utilisateur personne =new Utilisateur();
+
+        //personne.setAge(10);
+        //System.out.println( personne.toString());
+        //System.out.println("bonjour AGE"+personne.getAge());
+
     }
 
 }
