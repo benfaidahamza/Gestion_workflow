e70a0f630113e76b1f912902935c82804c133b31
hamza <benfaida.hamza2014@gmail.com>
2022-03-24 11:29:29 +0100

    commit1

---
+++ src/main/java/ma/thinline/gestion_workflow/GestionWorkflowApplication.java
@@ -0,0 +1,13 @@
+package ma.thinline.gestion_workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionWorkflowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionWorkflowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ma/thinline/gestion_workflow/GestionWorkflowApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.thinline.gestion_workflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionWorkflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
