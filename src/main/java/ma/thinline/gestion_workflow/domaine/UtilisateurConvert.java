fa3530b22f17f69ffbb85d4c24fc5ba5116e8388
hamza <benfaida.hamza2014@gmail.com>
2022-03-28 00:27:53 +0200

    uservo

--- src/main/java/ma/thinline/gestion_workflow/domaine/UtilisateurConvert.java
+++ src/main/java/ma/thinline/gestion_workflow/domaine/UtilisateurConvert.java
@@ -2,6 +2,9 @@ package ma.thinline.gestion_workflow.domaine;
 
 import ma.thinline.gestion_workflow.modele.Utilisateur;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UtilisateurConvert {
 
     public static UtilisateurVo toVo (Utilisateur user) {
@@ -20,5 +23,30 @@ public class UtilisateurConvert {
         vo.setPassword(user.getPassword());
         vo.setRole(user.getRole());
         return vo;
+    }
+
+    public static Utilisateur toBo (UtilisateurVo vo) {
+        if (vo == null || vo.getId_user() ==null)
+            return null;
+        Utilisateur user = new Utilisateur();
+        user.setId_user(vo.getId_user());
+        user.setAdresse(vo.getAdresse());
+        user.setAge(vo.getAge());
+        user.setPhone(vo.getPhone());
+        user.setCin(vo.getCin());
+        user.setEmail(vo.getEmail());
+        user.setDateNaissance(vo.getDateNaissance());
+        user.setFirst_name(vo.getFirst_name());
+        user.setLast_name(vo.getLast_name());
+        user.setPassword(vo.getPassword());
+        user.setRole(vo.getRole());
+        return user;
+    }
+    public static List<UtilisateurVo> toListVo (List <Utilisateur> listBo) {
+        List<UtilisateurVo> listVo = new ArrayList<>();
+        for (Utilisateur emp : listBo) {
+            listVo.add(toVo(emp));
+        }
+        return listVo;
     }
 }
